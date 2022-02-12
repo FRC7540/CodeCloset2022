f80e0130f1eab120a7026461c0dbef2d3ee59162
Lucasboi <lstafford@eastmountainhigh.net>
2022-02-12 12:20:16 -0700

    deleted old wpilib, added DriveBaseSubsystem

---
+++ src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveBaseSubsystem extends SubsystemBase {
+    /** Creates a new DriveBaseSubsystem. */
+    public DriveBaseSubsystem() {}
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
