b6cc0c7f7a5d7c67404ee75111b537537e11546a
kkocharoff <kkocharoff@eastmountainhigh.net>
2022-02-12 11:19:09 -0700

    Mechan Motor Set Up

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int kFrontLeftMotorCanID = 1;
+        public static final int kFrontRightMotorCanID = 2;
+        public static final int kRearLeftMotorCanID = 3;
+        public static final int kRearRightMotorCanID = 4;
+
+    }
+}
