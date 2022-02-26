6b7ef3126ce7618dc2e0561ac51e6876da45636c
KSatsujin <johnathan.sims360@gmail.com>
2022-02-26 15:35:20 -0700

    Changed CAN ID constants, added Intake constants, added intake control methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,17 +14,24 @@ public final class Constants {
     }
 
     public static final class ShooterConstants {
-        public static final int kShooterMotor1CanID = 8;
-        public static final int kShooterMotor2CanID = 9;
+        public static final int kShooterMotor1CanID = 31;
+        public static final int kShooterMotor2CanID = 32;
 
         public static final MotorType kMotorType = MotorType.kBrushed;
     }
     public static final class IntakeConstants {
-        public static final int kIntakeMotorCanID = 5; 
+        public static final int kIntakeSpoolMotorCanID = 11;
+        public static final int kIntakeRollerMotorCanID = 12;
+
+        public static final double kIntakeRollerMotorSpeed = 0.5;
+        public static final double kIntakeSpoolMotorSpeed = 0.5;
+
+        public static final int kIntakeLimitSwitchUp = 0;
+        public static final int kIntakeLimitSwitchDown = 1;
     }
     public static final class TowerConstants {
-        public static final int kFrontMotorCanID = 6;
-        public static final int kBackMotorCanID = 7;
+        public static final int kFrontMotorCanID = 21;
+        public static final int kBackMotorCanID = 22;
     }
     public static final class ClimberConstants {
         public static final int kClimberRightCanID = 69;
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,11 +8,21 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Add your docs here. */
 public class IntakeSubsystem extends SubsystemBase {
     /** Creates a new IntakeSubsystem. */
+
+    DigitalInput upLimitSwitch = new DigitalInput(Constants.IntakeConstants.kIntakeLimitSwitchUp);
+    DigitalInput downLimitSwitch = new DigitalInput(Constants.IntakeConstants.kIntakeLimitSwitchDown);
+
+    private static final WPI_VictorSPX spoolMotor = new WPI_VictorSPX(Constants.IntakeConstants.kIntakeSpoolMotorCanID);
+    private static final WPI_VictorSPX rollerMotor = new WPI_VictorSPX(Constants.IntakeConstants.kIntakeRollerMotorCanID);  
+
     public IntakeSubsystem() {}
 
     @Override
@@ -21,11 +31,30 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void intakeStop() {
-
+        rollerMotor.stopMotor();
     }
 
     // Note: isReverse should be FALSE in order to intake balls. TRUE makes it spit them out.
     public void intakeIn(boolean isReverse) {
+        if(isReverse){
+            rollerMotor.set(-Constants.IntakeConstants.kIntakeRollerMotorSpeed);
+        } else {
+            rollerMotor.set(Constants.IntakeConstants.kIntakeRollerMotorSpeed);
+        }
+    }
 
+    // Note: isUp should be TRUE to spool paracord. FALSE unspools, setting the intake rollers down.
+    public void intakePosition(boolean isUp) {
+        if(isUp && !upLimitSwitch.get()){
+            spoolMotor.set(Constants.IntakeConstants.kIntakeSpoolMotorSpeed);
+        } else if(!isUp && !downLimitSwitch.get()) {
+            spoolMotor.set(-Constants.IntakeConstants.kIntakeSpoolMotorSpeed);
+        } else {
+            spoolMotor.set(0);
+        }
+    }
+
+    public void intakeSpoolStop() {
+        spoolMotor.stopMotor();
     }
 }
