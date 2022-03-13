43f4562637b135ab53362bf60f297a54f7a00fe3
kkocharoff <kkocharoff@eastmountainhigh.net>
2022-03-12 20:09:15 -0700

    added LowerFeeder to autonomous script

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -87,6 +87,7 @@ public class RobotContainer {
 
     public void autonomous() {
         Command script = new SequentialCommandGroup(
+            new LowerFeeder(m_intake).withTimeout(5),
             new InstantCommand(() -> m_tower.setTowerSpeedManual(1), m_tower),
             new ParallelRaceGroup(
                 new RunCommand(() -> m_shooter.shooterVelocity(0.6), m_shooter).withTimeout(14), //keep running the shooter for the whole 15 second teleop
